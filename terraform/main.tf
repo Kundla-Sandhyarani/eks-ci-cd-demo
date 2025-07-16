dedae4da3816a6a1eff25dc8f25dcf0fb1ebc8e9
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-07-16 12:25:54 +0530

    teraform check

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,26 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "20.37.1" # or the version you're using
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "20.37.1"
 
   cluster_name    = "amazing-hiphop-wardrobe"
-  cluster_version = "1.29"
+  cluster_version = "1.33"
+
+  subnet_ids      = ["subnet-0c980a59bca3f2bc9", "subnet-038b4cd5e678d4ae9"]
   vpc_id          = "vpc-0121b1a9ddb0ffbfe"
 
-  subnet_ids = [
-    "subnet-0c980a59bca3f2bc9",
-    "subnet-038b4cd5e678d4ae9"
-  ]
+  # Add these lines ↓↓↓
+  cloudwatch_log_group_name     = "/aws/eks/amazing-hiphop-wardrobe/cluster"
+  create_cloudwatch_log_group   = false
 
-  eks_managed_node_group_defaults = {
-    instance_types = ["t3.small"]
-  }
+  # Optional: other config
+  enable_irsa   = true
 
   eks_managed_node_groups = {
     default = {
-      desired_size = 1
-      max_size     = 2
-      min_size     = 1
+      instance_types = ["t3.medium"]
+      desired_capacity = 2
+      min_capacity     = 1
+      max_capacity     = 3
     }
   }
 }
