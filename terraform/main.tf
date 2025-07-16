fb3dd99945e73b108dcaa65b4ab087fd51ed2633
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-07-16 12:33:07 +0530

    tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,12 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws", version = ">=5.0" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.100.0"
+    }
   }
+
+  required_version = ">= 1.3.0"
 }
 
 provider "aws" {
@@ -17,19 +22,15 @@ module "eks" {
   vpc_id          = "vpc-0121b1a9ddb0ffbfe"
   subnet_ids      = ["subnet-0c980a59bca3f2bc9", "subnet-038b4cd5e678d4ae9"]
 
-  create_cloudwatch_log_group = false  # Reuse existing log group
+  enable_cluster_creator_admin_permissions = true
+  create_cloudwatch_log_group = false
 
-  # For IRSA and Node Group configuration:
-  manage_cluster_iam = false  # disable auto IAM creation
-  manage_aws_auth    = true
-
-  # Use built-in node_groups block instead of eks_managed_node_groups
   node_groups = {
     default = {
-      desired_capacity = 1
-      min_capacity     = 1
-      max_capacity     = 2
-      instance_types   = ["t3.medium"]
+      desired_size   = 1
+      max_size       = 2
+      min_size       = 1
+      instance_types = ["t3.medium"]
     }
   }
 }
