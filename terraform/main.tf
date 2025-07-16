f8da49dbbb278d0e8aaa955d94105baaeb3571e9
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-07-16 12:19:30 +0530

    Update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-southeast-2"
 }
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "eks-ci-cd-cluster"
   cluster_version = "1.27"
-  subnets         = ["subnet-abc", "subnet-def"]
-  vpc_id          = "vpc-xyz"
+  subnets         = ["subnet-0c980a59bca3f2bc9", "subnet-038b4cd5e678d4ae9"]
+  vpc_id          = "vpc-0121b1a9ddb0ffbfe"
 }
